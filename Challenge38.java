d5aacf79e9c087d900a2367f813da9499a103a22
Anil Kumar <84949887+anil07-web@users.noreply.github.com>
2021-10-28 19:28:58 +0530

    Update Challenge38.java

--- Challenge38.java
+++ Challenge38.java
@@ -2,13 +2,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.awt.*;
 
-public class Challenge38 remoetly1 sddd lolo
-{
-<<<<<<< Updated upstream
-    public static void main(String[] args) remotely2 remotely2 remotely3 remotely4 develop change
-=======
+public class Challenge38 {
+
     public static void main(String[] args) remotely2 remotely2 remotely3 local4
->>>>>>> Stashed changes
+
     {
         BinaryTree bin = new BinaryTree();
         BufferedReader buff = null;
