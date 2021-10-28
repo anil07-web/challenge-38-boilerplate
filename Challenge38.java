ced69747efeed17358204f93afea91510ab6aefc
Anil Kumar <84949887+anil07-web@users.noreply.github.com>
2021-10-28 19:39:07 +0530

    Update Challenge38.java

--- Challenge38.java
+++ Challenge38.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public class Challenge38 {
 
-    public static void main(String[] args) remotely2 remotely2 remotely3 local4 develop develop2
+    public static void main(String[] args) remotely2 remotely2 remotely3 local4 develop develop2 dev2
 
     {
         BinaryTree bin = new BinaryTree();
